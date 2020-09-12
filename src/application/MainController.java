5d96cd6a4218460119804ca17732fea97c938f87
kumsal67 <70024440+kumsal67@users.noreply.github.com>
2020-09-12 20:21:03 +0300

    fıxıt

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,46 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class MainController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane kokMain;
+
+    @FXML
+    void cikti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hesaplama(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kayit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void raporlama(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+    	kokMain.toFront();
+        assert kokMain != null : "fx:id=\"kokMain\" was not injected: check your FXML file 'Main_Menu.fxml'.";
+
+    }
 }
